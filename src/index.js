b1c9985d2d024d693d0e76e3aa1e4af47a160d41
Nazar Matviyiv <nazar@tlt.local>
2018-07-07 11:03:59 +0400

    Update manifest

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// ReactDOM.render(<Login />, document.getElementById('root'));
-
 // TODO: to have ServiceWorker in production we need to host assets
 registerServiceWorker();
 
